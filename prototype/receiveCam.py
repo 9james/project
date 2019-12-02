a5e8a1acd5488b8dc75cca83a5d4b28f95699356
9james <57711075+9james@users.noreply.github.com>
2019-12-02 13:05:16 +0900

    Add files via upload

---
+++ prototype/receiveCam.py
@@ -0,0 +1,85 @@
+import flask
+from flask import Flask, request, render_template, url_for
+import numpy as np
+import cv2 as cv
+import os
+from flask import Response
+from werkzeug.utils import secure_filename
+import threading
+import socket
+
+outputFrame=None
+lock=threading.Lock()
+
+HOST='127.0.0.1'
+PORT=5000
+
+
+app=Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+def recvall(sock,count):
+    buf=b''
+    while count:
+        newbuf=sock.recv(count)
+        if not newbuf: return None
+        buf+=newbuf
+        count-=len(newbuf)
+    return buf
+
+def recv():
+    global outputFrame, lock
+    client_socket=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    print("a")
+    client_socket.connect((HOST,PORT))
+    print("b")
+    while True:
+        message='1'
+        client_socket.send(message.encode())
+
+        length=recvall(client_socket,16)
+        stringData=recvall(client_socket,int(length))
+        data=np.frombuffer(stringData,dtype='uint8')
+
+        decimg=cv.imdecode(data,1)
+       # encodeImage = cv.imencode(".jpg", decimg)
+       # print('c')
+        with lock:
+            outputFrame = decimg.copy()
+       # yield(b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' + bytearray(decimg) + b'\r\n')
+
+def generate():
+    global outputFrame, lock
+
+    # loop over frames from the output stream
+    while True:
+    # wait until the lock is acquired
+       # print('e')
+        with lock:
+            if outputFrame is None:
+                continue
+            (flag, encodedImage) = cv.imencode(".jpg", outputFrame)
+            if not flag:
+                continue
+
+        yield(b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' + bytearray(encodedImage) + b'\r\n')
+
+@app.route('/video_feed')
+def video_feed():
+    # return the response generated along with the specific media
+    # type (mime type)
+    print('d')
+    return Response(generate(),mimetype = "multipart/x-mixed-replace; boundary=frame")
+
+
+
+
+
+if __name__ == '__main__':
+    t = threading.Thread(target=recv)
+    t.daemon = True
+    t.start()
+    app.run(host='0.0.0.0', port=5555, debug=True,threaded=True,use_reloader=False)
